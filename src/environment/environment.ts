0b6eeefa2a2c0b25725ea75fe7f41013065c3a6f
Gavindu98 <rathnayakagavindu98@gmail.com>
2022-12-18 14:44:59 +0530

    color changed

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,7 @@
 // Remame file as .tsx. Remove this line also.
 export const environment = {
-    api_url: "http://localhost:8080",
+    api_url: "https://e-guru-api-test.onrender.com",
     app_url: "http://localhost:3000",
+    // api_url: "http://localhost:8080",
+    // app_url: "http://localhost:3000",
 };
-  
