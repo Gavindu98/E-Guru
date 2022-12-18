d4bfe8805eaeb680f8595c39c921ff3e012c6db4
Gavindu98 <rathnayakagavindu98@gmail.com>
2022-12-18 15:37:19 +0530

    enviroment updated

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,6 +2,4 @@
 export const environment = {
     api_url: "https://e-guru-api-test.onrender.com",
     app_url: "http://localhost:3000",
-    // api_url: "http://localhost:8080",
-    // app_url: "http://localhost:3000",
 };
